03bbadf8dbdf6ed5bacceacbde5ec8fa1fb6e6ff
BENALIOUA Karima <karimabenalioua9@gmail.com>
2022-03-11 16:57:07 +0100

    calculator

--- calcul.js
+++ calcul.js
@@ -1,6 +1,8 @@
 // var
 var ecran = document.getElementById("ecran");
 var numChar;
+var currentChar, previousChar;
+var operatons = ['+', '-', '*', '/', '.'];
 //function
 
 function clearscreen() {
@@ -10,12 +12,38 @@ function clearscreen() {
 function display(c) {
     ecran.value += c;
     numChar = ecran.value.length;
+    currentChar = c;
+    getpreviousChar();
 }
 
-function calculate() {
+function operate() {
     ecran.value = eval(ecran.value);
 }
 
 function back() {
     ecran.value = ecran.value.substr(0, ecran.value.length - 1);
+}
+
+function getpreviousChar() {
+    previousChar = ecran.value.substring(numChar - 2, numChar - 1);
+    double();
+}
+
+function double() {
+    if (operatons.includes(previousChar) && operatons.includes(currentChar)) {
+        if (previousChar == currentChar) {
+            deleteChar();
+        } else {
+            remplaceoperate();
+        }
+    }
+}
+
+function remplaceoperate() {
+    ecran.value = ecran.value.slice(0, numChar - 2) + ecran.value.slice(numChar - 1);
+
+}
+
+function deleteChar() {
+    ecran.value = ecran.value.substring(0, numChar - 1);
 }
